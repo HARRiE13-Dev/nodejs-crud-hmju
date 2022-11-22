fd399d1910e34c3acfdbafca3a639301ab1413d5
harrie-jetcommerce <surachart.k@jetcommerce.co.th>
2022-11-22 12:03:29 +0700

    Day 1 : Theme Template

--- app.js
+++ app.js
@@ -1,62 +1,74 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 //Extention for use data
-let flash = require('express-flash');
-let session = require('express-session');
-let mysql = require('mysql');
-let connection = require('./lib/Database')
+let flash = require("express-flash");
+let session = require("express-session");
+let mysql = require("mysql");
+let connection = require("./lib/Database");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 // Page Input
-var newsRouter = require('./routes/news');
+var newsRouter = require("./routes/news");
+var aboutRouter = require("./routes/about");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Call to use extention
-app.use(session({
-  cookie :{ maxAge : 6000},
-  store: new session.MemoryStore,
-  saveUninitialized:true,
-  resave:'true',
-  secret:'secret'
-}))
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    store: new session.MemoryStore(),
+    saveUninitialized: true,
+    resave: "true",
+    secret: "secret",
+  })
+);
 
-app.use(flash())
+app.use(flash());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/news", newsRouter);
+app.use("/about", aboutRouter);
+app.use("/service", aboutRouter);
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/news', newsRouter);
+// import File
+app.use(express.static("img"));
+app.use(express.static("css"));
+app.use(express.static("js"));
+app.use(express.static("scss"));
+app.use(express.static("lib"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/news.js
+++ routes/news.js
@@ -7,10 +7,59 @@ router.get("/", (req, res, next) => {
   dbCon.query("SELECT * FROM news ORDER BY ID desc", (err, rows) => {
     if (err) {
       req.flash("error", err);
-      res.render("news", { data: '' });
+      res.render("news", { data_news: "" });
     } else {
-      res.render("news", { data: rows });
+      res.render("news", { data_news: rows });
     }
   });
 });
+
+// display add book page
+router.get("/add", (req, res, next) => {
+  res.render("news/add", {
+    Topic: "",
+    Detail_1: "",
+  });
+});
+
+// add a new book
+router.post("/add", (req, res, next) => {
+  let Topic = req.body.Topic;
+  let Detail_1 = req.body.Detail_1;
+  let errors = false;
+
+  if (Topic.length === 0 || Detail_1.length === 0) {
+    errors = true;
+    // set flash message
+    req.flash("error", "Please enter name and author");
+    // render to add.ejs with flash message
+    res.render("news/add", {
+      Topic: Topic,
+      Detail_1: Detail_1,
+    });
+  }
+
+  // if no error
+  if (!errors) {
+    let form_data = {
+      Topic: Topic,
+      Detail_1: Detail_1,
+    };
+
+    // insert query
+    dbCon.query("INSERT INTO news SET ?", form_data, (err, result) => {
+      if (err) {
+        req.flash("error", err);
+        res.render("news/add", {
+          Topic: form_data.Topic,
+          Detail_1: form_data.Detail_1,
+        });
+      } else {
+        req.flash("success", "Book successfully added");
+        res.redirect("/news");
+      }
+    });
+  }
+});
+
 module.exports = router;
